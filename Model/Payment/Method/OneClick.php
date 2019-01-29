88acd1014897cb102e340d540de1b73e36b44ccb
Matthieu BOUCHOT <matthieu.bouchot@oyst.com>
2019-01-29 16:26:49 +0100

    Refund Refactoring

--- Model/Payment/Method/OneClick.php
+++ Model/Payment/Method/OneClick.php
@@ -17,10 +17,53 @@ class OneClick extends \Magento\Payment\Model\Method\AbstractMethod
 
     protected $_canRefundInvoicePartial = true;
 
+    protected $oystPaymentManagement;
+
+    public function __construct(
+        \Oyst\OneClick\Model\OystPaymentManagement $oystPaymentManagement,
+        \Magento\Framework\Model\Context $context,
+        \Magento\Framework\Registry $registry,
+        \Magento\Framework\Api\ExtensionAttributesFactory $extensionFactory,
+        \Magento\Framework\Api\AttributeValueFactory $customAttributeFactory,
+        \Magento\Payment\Helper\Data $paymentData,
+        \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig,
+        \Magento\Payment\Model\Method\Logger $logger,
+        \Magento\Framework\Model\ResourceModel\AbstractResource $resource = null,
+        \Magento\Framework\Data\Collection\AbstractDb $resourceCollection = null,
+        array $data = [],
+        \Magento\Directory\Helper\Data $directory = null
+    ) {
+        parent::__construct(
+            $context,
+            $registry,
+            $extensionFactory,
+            $customAttributeFactory,
+            $paymentData,
+            $scopeConfig,
+            $logger,
+            $resource,
+            $resourceCollection,
+            $data,
+            $directory
+        );
+        $this->oystPaymentManagement = $oystPaymentManagement;
+    }
+
     public function isActive($storeId = null)
     {
         $path = 'oyst_oneclick/general/enabled';
         $result = $this->_scopeConfig->getValue($path, \Magento\Store\Model\ScopeInterface::SCOPE_STORE, $storeId);
         return (bool)(int)$result;
+    }
+
+    public function refund(\Magento\Payment\Model\InfoInterface $payment, $amount)
+    {
+        $creditmemo = $payment->getCreditmemo();
+        $order = $payment->getOrder();
+        $this->oystPaymentManagement->handleMagentoOrdersToRefund(
+            [$order->getId() => $creditmemo->getBaseGrandTotal()], true
+        );
+
+        return $this;
     }
 }
